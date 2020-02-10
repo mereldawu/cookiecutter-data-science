255ad56244591cf51480734be686036a3818c69b
Merelda <merelda.wu@standardbank.co.za>
2020-02-10 10:58:55 +0200

    Updated /opt/play to /opt/app

--- {{ cookiecutter.repo_name }}/config/run.sh
+++ {{ cookiecutter.repo_name }}/config/run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-mkdir -p /home/jovyan/opt/play/config/
+mkdir -p /home/jovyan/opt/app/config/
 mkdir -p /home/jovyan/.jupyter/lab/user-settings/\@jupyterlab/shortcuts-extension/
 mkdir -p /home/jovyan/.jupyter/lab/user-settings/\@jupyterlab/codemirror-extension/
 mkdir -p /home/jovyan/.ipython/profile_default/startup/
 
-cp /opt/play/config/plugin.jupyterlab-settings ~/.jupyter/lab/user-settings/\@jupyterlab/shortcuts-extension/
-cp /opt/play/config/commands.jupyterlab-settings ~/.jupyter/lab/user-settings/@jupyterlab/codemirror-extension/
-echo "$(</opt/play/config/jupyter_notebook_config.py)" >> /home/jovyan/.jupyter/jupyter_notebook_config.py
-echo "$(</opt/play/config/start.py)" >> /home/jovyan/.ipython/profile_default/startup/start.py
+cp /opt/app/config/plugin.jupyterlab-settings ~/.jupyter/lab/user-settings/\@jupyterlab/shortcuts-extension/
+cp /opt/app/config/commands.jupyterlab-settings ~/.jupyter/lab/user-settings/@jupyterlab/codemirror-extension/
+echo "$(</opt/app/config/jupyter_notebook_config.py)" >> /home/jovyan/.jupyter/jupyter_notebook_config.py
+echo "$(</opt/app/config/start.py)" >> /home/jovyan/.ipython/profile_default/startup/start.py
